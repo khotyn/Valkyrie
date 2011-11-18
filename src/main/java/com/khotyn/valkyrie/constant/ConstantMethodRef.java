088596c6c2e6f7ae8ac564dfe65db6ff350c462a
khotyn <hting1@gmail.com>
2011-11-18 23:14:57 +0800

    Add some constant pool parse code.

---
+++ src/main/java/com/khotyn/valkyrie/constant/ConstantMethodRef.java
@@ -0,0 +1,10 @@
+package com.khotyn.valkyrie.constant;
+
+/**
+ * User: apple
+ * Date: 11-11-18
+ * Time: PM10:13
+ * Dust to dust, earth to earth.
+ */
+public class ConstantMethodRef {
+}
---
+++ src/main/java/com/khotyn/valkyrie/constant/ConstantNameAndType.java
@@ -0,0 +1,10 @@
+package com.khotyn.valkyrie.constant;
+
+/**
+ * User: apple
+ * Date: 11-11-18
+ * Time: PM11:03
+ * Dust to dust, earth to earth.
+ */
+public class ConstantNameAndType {
+}
